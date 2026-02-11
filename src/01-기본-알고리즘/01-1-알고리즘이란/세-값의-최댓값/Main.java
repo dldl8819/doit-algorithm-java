3a97f056af4786c4d7bfb7d0f0b19e639bf376da
mslee <mslee@cudo.co.kr>
2026-02-11 16:36:30 +0900

    Update max-of-three example Main.java

--- src/01-기본-알고리즘/01-1-알고리즘이란/세-값의-최댓값/Main.java
+++ src/01-기본-알고리즘/01-1-알고리즘이란/세-값의-최댓값/Main.java
@@ -23,5 +23,60 @@ class Ch01S1T01 {
     }
 }
 
+class Max3m {
+    static int max3(int a, int b, int c) {
+        int max = a;
+        if (b > max) max = b;
+        if (c > max) max = c;
 
+        return max;
+    }
+    public static void main(String[] args) {
+        System.out.println("세 정수의 최댓값을 구합니다.");
+        System.out.println("최댓값은 " + max3(10, 20, 30) + "입니다.");
+    }
+}
 
+class Max4m {
+    static int max4(int a, int b, int c, int d) {
+        int max = a;
+        if (b > max) max = b;
+        if (c > max) max = c;
+        if (d > max) max = d;
+
+        return max;
+    }
+    public static void main(String[] args) {
+        System.out.println("네 정수의 최댓값을 구합니다.");
+        System.out.println("최댓값은 " + max4(10, 20, 30, 40) + "입니다.");
+    }
+}
+
+class Min3 {
+    static int min3(int a, int b, int c) {
+        int min = a;
+        if (b < min) min = b;
+        if (c < min) min = c;
+
+        return min;
+    }
+    public static void main(String[] args) {
+        System.out.println("세 정수의 최솟값을 구합니다.");
+        System.out.println("최솟값은 " + min3(10, 20, 30) + "입니다.");
+    }
+}
+
+class Min4 {
+    static int min4(int a, int b, int c, int d) {
+        int min = a;
+        if (b < min) min = b;
+        if (c < min) min = c;
+        if (d < min) min = d;
+
+        return min;
+    }
+    public static void main(String[] args) {
+        System.out.println("네 정수의 최솟값을 구합니다.");
+        System.out.println("최솟값은 " + min4(10, 20, 30, 5) + "입니다.");
+    }
+}
