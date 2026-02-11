c98867fc2efbc1772682df9a2f88204b1ff6cbcd
mslee <mslee@cudo.co.kr>
2026-02-11 17:06:51 +0900

    Implement sign-judgment branching example

--- src/01-기본-알고리즘/01-1-알고리즘이란/조건-판단과-분기/Main.java
+++ src/01-기본-알고리즘/01-1-알고리즘이란/조건-판단과-분기/Main.java
@@ -4,7 +4,18 @@
  */
 class Ch01S1T02 {
     public static void main(String[] args) {
-        // TODO: implement
+        try (Scanner stdIn = new Scanner(System.in)) {
+            System.out.print("정수를 입력하세요: ");
+            int n = stdIn.nextInt();
+
+            if (n > 0) {
+                System.out.println("양수입니다.");
+            } else if (n < 0) {
+                System.out.println("음수입니다.");
+            } else {
+                System.out.println("0입니다.");
+            }
+        }
     }
 }
 
